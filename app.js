4e5ab0c8f883eb616dcc3542cb988a867be4d35c
Inayath Ali <inayath5026@gmail.com>
2024-10-27 00:53:32 +0530

    Implement listings and show route functionality

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 const PORT = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+
 main()
 .then(()=>{
     console.log("Conneted to DB.");
@@ -19,9 +23,22 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+// Listings Route
+app.get('/listings', async(req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+});
+
+// show Route
+app.get('/listings/:id', async(req,res)=>{
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+
 app.get('/', (req,res)=>{
     res.send("Root Route.");
-} );
+});
 
 app.listen(PORT , ()=>{
     console.log(`App is Listening to PORT : ${PORT}`);
