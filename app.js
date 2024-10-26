1277016c9684ae8f22707243d6faa5b3f5446515
Inayath Ali <inayath5026@gmail.com>
2024-10-27 01:29:44 +0530

    Implement new and create route functionality

--- app.js
+++ app.js
@@ -29,11 +29,23 @@ app.get('/listings', async(req,res)=>{
     res.render("listings/index.ejs", {allListings});
 });
 
-// show Route
+// New Route
+app.get('/listings/new', (req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+// Show Route
 app.get('/listings/:id', async(req,res)=>{
     const {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
+});
+
+// Create Route
+app.post('/listings', async (req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
 });
 
 app.get('/', (req,res)=>{
